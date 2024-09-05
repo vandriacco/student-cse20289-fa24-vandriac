33ef39b61b3f698308607c018c6dc053b11af2f2
Vince Andriacco <vandriac@nd.edu>
2024-09-04 22:49:04 -0400

    hw01: finished simplefunc and added comments

--- hw/hw01/searchsrc.py
+++ hw/hw01/searchsrc.py
@@ -1,12 +1,18 @@
+# Vince Andriacco
+# vandriac@nd.edu
+
 import argparse
 
+# reads file into list of strings
 def readFile(filename: str):
     file = open(filename, 'r')
     return file.readlines()
 
+# counts number of lines in file
 def countLine(fileLines):
     return len(fileLines)
 
+# counts number of include statements
 def countInclude(fileLines):
     num_includes = 0
     
@@ -16,6 +22,7 @@ def countInclude(fileLines):
 
     return num_includes
 
+# counts number of member functions
 def countMember(fileLines):
     num_members = 0
 
@@ -24,6 +31,7 @@ def countMember(fileLines):
 
     return num_members
 
+# counts number of pointer dereferences
 def countPointer(fileLines):
     num_ptrs = 0
 
@@ -32,7 +40,27 @@ def countPointer(fileLines):
 
     return num_ptrs
 
+# counts functions with one line
+def countSimpleFunc(fileLines):
+    num_simplefunc = 0
 
+    for i in range(len(fileLines)):
+        # checks if line before and after logic of function only has a curly brace (and new line)
+        if fileLines[i].startswith('{') and fileLines[i+2].startswith('}'):
+            num_simplefunc += 1
+
+    return num_simplefunc
+
+# counts functions with one line
+def countSimpleFuncEC(fileLines):
+    num_simplefunc = 0
+
+    for i in range(len(fileLines)):
+        # counts curly brace regardless of whether it is on its own line
+        if '{' in fileLines[i] and '}' in fileLines[i+2]:
+            num_simplefunc += 1
+
+    return num_simplefunc
 
 # Define the various arguments via argparse
 parser = argparse.ArgumentParser(description='Analyze c++ files')
@@ -47,9 +75,14 @@ args = parser.parse_args()
 
 file = readFile(args.file_name)
 
+# chooses correct output based on flag given
 if args.include:
     print('file:', args.file_name, 'lines:', countLine(file), 'include:', countInclude(file))
 elif args.member:
     print('file:', args.file_name, 'lines:', countLine(file), 'member:', countMember(file))
 elif args.ptr:
     print('file:', args.file_name, 'lines:', countLine(file), 'ptr:', countPointer(file))
+elif args.simplefunc:
+    print('file:', args.file_name, 'lines:', countLine(file), 'simplefunc:', countSimpleFunc(file))
+elif args.simplefuncec:
+    print('file:', args.file_name, 'lines:', countLine(file), 'simplefunc:', countSimpleFuncEC(file))
