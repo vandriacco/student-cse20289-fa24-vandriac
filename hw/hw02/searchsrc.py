12a58b872b0c2e49f71e1a912f3e4b0b7a7a9920
Vince Andriacco <vandriac@nd.edu>
2024-09-18 22:33:23 -0400

    hw02: updated searchsrc.py

--- hw/hw02/searchsrc.py
+++ hw/hw02/searchsrc.py
@@ -2,6 +2,7 @@
 # vandriac@nd.edu
 
 import argparse
+import re
 
 # reads file into list of strings
 def readFile(filename: str):
@@ -22,70 +23,66 @@ def countInclude(fileLines):
 
     return num_includes
 
+def countIncludeLocal(fileLines):
+    num_includes = 0
+    
+    for line in fileLines:
+        if line.startswith("#include") and line.split()[1].startswith("\""):
+            num_includes += 1
+
+    return num_includes
+
 # counts number of member functions
-def countMember(fileLines):
+def countMemberFuncs(fileLines):
     num_members = 0
 
     for line in fileLines:
-        num_members += line.count('::')
+        if re.search("^[a-zA-Z0-9].*(::)", line):
+            num_members += 1
 
     return num_members
 
-# counts number of pointer dereferences
-def countPointer(fileLines):
-    num_ptrs = 0
-
-    for line in fileLines:
-        num_ptrs += line.count('->')
-
-    return num_ptrs
-
 # counts functions with one line
-def countSimpleFunc(fileLines):
+def countOneLineFunc(fileLines):
     num_simplefunc = 0
 
     for i in range(len(fileLines)):
         # checks if line before and after logic of function only has a curly brace (and new line)
-        if fileLines[i].startswith('{') and fileLines[i+2].startswith('}'):
-            num_simplefunc += 1
+        if re.search("^[a-zA-Z0-9].*[{]$", fileLines[i]):
+            if re.search("\n$", fileLines[i+1]):
+                if re.search("^}", fileLines[i+2]):
+                    num_simplefunc += 1
 
     return num_simplefunc
 
-# counts functions with one line
-def countSimpleFuncEC(fileLines):
-    num_simplefunc = 0
-
-    for i in range(len(fileLines)):
-        # counts curly brace regardless of whether it is on its own line
-        if '{' in fileLines[i] and '}' in fileLines[i+2]:
-            num_simplefunc += 1
-
-    return num_simplefunc
 
 # Define the various arguments via argparse
 parser = argparse.ArgumentParser(description='Analyze c++ files')
 parser.add_argument('file_name', help='Name of file to analyze')
 parser.add_argument('--include', help='Count number of includes', action='store_true')
-parser.add_argument('--member', help='Count number of member functions', action='store_true')
-parser.add_argument('--ptr', help='Count pointer references ->', action='store_true')
-parser.add_argument('--simplefunc', help='Count functions with one line', action='store_true')
-parser.add_argument('--simplefuncec', help='Count functions with one line extra credit', action='store_true')
+parser.add_argument('--includelocal', help='Count number of local includes', action='store_true')
+parser.add_argument('--memberfuncs', help='Count number of member functions', action='store_true')
+parser.add_argument('--onelinefuncs', help='Count functions with one line', action='store_true')
 
 args = parser.parse_args()
 
 file = readFile(args.file_name)
 
 # chooses correct output based on flag given
-print('file:', args.file_name)
+
+path = args.file_name.split('/')
+file_name = path.pop()
+path = '/'.join(path) + '/'
+
+print('path:', path)
+print('file:', file_name)
 print('lines:', countLine(file))
 
 if args.include:
     print('include:', countInclude(file))
-if args.member:
-    print('member:', countMember(file))
-if args.ptr:
-    print('ptr:', countPointer(file))
-if args.simplefunc:
-    print('simplefunc:', countSimpleFunc(file))
-if args.simplefuncec:
-    print('simplefunc:', countSimpleFuncEC(file))
+if args.includelocal:
+    print('includelocal:', countIncludeLocal(file))
+if args.memberfuncs:
+    print('memberfuncs:', countMemberFuncs(file))
+if args.onelinefuncs:
+    print('onelinefuncs:', countOneLineFunc(file))
